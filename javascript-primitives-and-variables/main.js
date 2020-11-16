b3a2685ac49a642b55a1c87d6d3e27ab0eb8d1b9
josh choi <wonchoi1994@gmail.com>
2020-11-16 13:08:16 -0800

    completed js prims and vars

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+var fullName="Josh Choi";
+console.log("value of full name:", fullName);
+
+var isCool="Cool";
+isCool=true;
+console.log("value of isCool:", isCool);
+
+var totalPets=1;
+console.log("value of totalPets:", totalPets)
+
+console.log("typeof fullName:", typeof 'Josh Choi');   
+
+console.log("typeof isCool:", typeof true);
+
+console.log("typeof totalPets:", typeof 1);
+
+var unknown=null;
+console.log("value of unknown:", null);
+
+var never;
+console.log("value of never:", never);
+
+console.log("typeof never:", typeof never);
