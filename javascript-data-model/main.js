564d95ff11eb0b0c526228b944eddc3da43f6ac9
josh choi <wonchoi1994@gmail.com>
2020-11-17 17:27:04 -0800

    complete javascript data model

---
+++ javascript-data-model/main.js
@@ -0,0 +1,76 @@
+/* exported orderHistory */
+
+var orderHistory = [
+  {
+    orderNumber: "114-3941689-8772232",
+    orderPlaced: "August 4 2020",
+    shipTo: "JS Masher",
+    delivered: "Aug 8 2020",
+    image: "https://javascriptforimpatientprogrammers.jpg",
+    items: [
+      {
+        type: "book",
+        title: "JavasScript for Impatient programmers",
+        author: "Rauschmayer, Dr.Axel",
+        returnBy: "Sep 7, 2020",
+        cost: "$34.00",
+      },
+    ],
+  },
+  {
+    orderNumber: "113-9984268-1280257",
+    orderPlaced: "July 19 2020",
+    shipTo: "JS Masher",
+    delivered: "Jul 20 2020",
+    image: "https://timelesswayofbuilding.jpg",
+    items: [
+      {
+        type: "book",
+        title: "The Timeless Way of Building",
+        author: "Alexander, Christopher",
+        returnBy: "Aug 19 2020",
+        cost: "$44.53",
+      },
+    ],
+  },
+  {
+    orderNumber: "114-2875557-9059409",
+    orderPlaced: "July 4 2020",
+    shipTo: "JS Masher",
+    delivered: "Jul 7 2020",
+    image: "https://gamecubecontrolleradapter.jpg",
+    items: [
+      {
+        type: "hardware",
+        title:
+          "Gamecube controller adapter, super smash bros switch gamecube adaapter for WII U, PC, Support Turbo and vibration features. No Driver and No Lag-Gamecube Adapter",
+        returnBy: "Aug 5, 2020",
+        cost: "$17.22",
+      },
+    ],
+  },
+  {
+    orderNumber: "113-2883177-2648248",
+    orderPlaced: "July 3 2020",
+    shipTo: "JS Masher",
+    delivered: "Jul 5 2020",
+    image: ["https://gamecubecontroller.jpg", "https://theartofsql.jpg"],
+    items: [
+      {
+        type: "hardware",
+        title:
+          "GameCube Controller - Supersmash bros. Edition(Nintendo Switch)",
+        returnBy: "Aug 4 2020",
+        cost: "$94.94",
+      },
+    ][
+      {
+        type: "book",
+        title: "The Art of SQL",
+        author: "Faroult, Stephane",
+        returnBy: "Aug 4 2020",
+        cost: "$33.99",
+      }
+    ],
+  },
+];
