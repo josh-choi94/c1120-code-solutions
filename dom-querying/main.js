f1ab2aedb40b1ee31cdde1f9793e0214a1ef3173
josh choi <wonchoi1994@gmail.com>
2020-11-23 11:02:43 -0800

    complete dom querying

---
+++ dom-querying/main.js
@@ -0,0 +1,19 @@
+var el = document.querySelector('h1');
+console.log(document.querySelector('h1'));
+console.dir(el);
+
+var p = document.querySelector('p');
+console.log(document.querySelector('p'));
+console.dir(p);
+
+var hint = document.querySelector('p.hint');
+console.log(document.querySelector('p.hint'));
+console.dir(hint);
+
+var ptwo = document.querySelectorAll('p');
+console.log(document.querySelectorAll('p'));
+console.dir(ptwo);
+
+var example = document.querySelectorAll('a.example-link');
+console.log(document.querySelectorAll('a.example-link'));
+console.dir(example);
