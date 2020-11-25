79345d1763c340de85446f56d4178aead59d168c
josh choi <wonchoi1994@gmail.com>
2020-11-24 17:07:51 -0800

    event listener changed

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,11 +1,10 @@
 var modal = document.getElementById("theModal");
 var button = document.getElementById("open");
-var noButton = document.getElementById("no");
+var noButton = document.getElementById("no").addEventListener("click", function (event) {
+    modal.style.display = "none";
+},
 
 button.onclick = function (event) {
   modal.style.display = "block";
 
-  noButton.addEventListener("click", function (event) {
-    modal.style.display = "none";
-  });
-};
+})
